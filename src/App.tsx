aa207907d9973fcec7a4bf332cce7baff9893dbd
thao phuong <dophuongthao.459@gmail.com>
2021-11-24 23:46:44 +0700

    Test Ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Button} from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="primary">Primary Button</Button>
+      <Button>Default Button</Button>
+      <Button type="dashed">Dashed Button</Button>
+      <br />
+      <Button type="text">Text Button</Button>
+      <Button type="link">Link Button</Button>
     </div>
   );
 }
